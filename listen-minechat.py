f8ccf89b41caf096fd8a8d5d1e663c8cfe72ea40
yk <yefim_korshever@mail.ru>
2022-10-29 15:18:49 +0300

    Add CLI options

--- listen-minechat.py
+++ listen-minechat.py
@@ -1,15 +1,40 @@
+import argparse
 import asyncio
 import datetime
 import socket
 import sys
 
 import aiofiles
+from environs import Env
 
 
-async def read_chat():
-    file_path = 'history.txt'
-    host = 'minechat.dvmn.org'
-    port = 5000
+def create_args_parser():
+    description = ('Listen to Minecraft chat.')
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        '--host',
+        metavar='{host}',
+        help='The chat host, minechat.dvmn.org by default',
+    )
+
+    parser.add_argument(
+        '--port',
+        metavar='{port}',
+        help='The chat port, 5000 by default',
+        type=int,
+    )
+
+    parser.add_argument(
+        '--file_path',
+        metavar='{file path}',
+        help='A path to history file, history.txt by default',
+    )
+
+    return parser
+
+
+async def read_chat(host, port, file_path):
     reader = writer = None
     date_format = '%d.%m.%y %H:%M'
     error_delay = 1
@@ -56,4 +81,24 @@ async def read_chat():
     writer.close()
 
 if __name__ == '__main__':
-    asyncio.run(read_chat())
+    env = Env()
+    env.read_env()
+    args_parser = create_args_parser()
+    args = args_parser.parse_args()
+    with env.prefixed('LISTEN_'):
+        if args.host:
+            host = args.host
+        else:
+            host = env('HOST', 'minechat.dvmn.org')
+
+        if isinstance(args.port, int):
+            port = args.port
+        else:
+            port = env.int('PORT', 5000)
+
+        if args.file_path:
+            file_path = args.file_path
+        else:
+            file_path = env('FILE', 'history.txt')
+
+    asyncio.run(read_chat(host, port, file_path))
